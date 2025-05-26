28a27a44f0c8d91fd2ddc6aaa901e6c4247087a6
JetixL11 <pavzlo17@yandex.ru>
2025-05-26 12:23:38 +0300

    Решение 2 модуля

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,18 @@
 function isEmpty(obj) {
   // ваш код...
+ for (let key in obj) {
+    // Учитываем только собственные свойства объекта (не унаследованные)
+    if (obj.hasOwnProperty(key)) {
+      return false;
+    }
+  }
+  return true;
 }
+
+let schedule = {};
+
+alert ( isEmpty(schedule) );
+
+schedule["8:30"] = "подъём";
+
+alert ( isEmpty(schedule) );
