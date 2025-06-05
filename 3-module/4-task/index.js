652c6d0213bc2d327469121448391f7f2b524b24
JetixL11 <pavzlo17@yandex.ru>
2025-06-05 15:41:35 +0300

    3 MODULE, 4 Таск не проходит тесты, не понимаю в чем дело, уже и в нейронку вгонял, не понимаю в чем ошибка

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,54 @@
 function showSalary(users, age) {
-  // ваш код...
+  const filteredUsers = users.filter(user => user.age <= age);
+  const resultLines = filteredUsers.map(user => `${user.name}, ${user.balance}`);
+  return resultLines.join('\n');
 }
+
+let user1 = {
+  "balance": "$7,825.65",
+  "picture": "https://placehold.it/32x32",
+  "age": 21,
+  "name": "Golden Branch",
+  "gender": "male",
+  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+  "favouriteFruit": "banana"
+};
+
+let user2 = {
+  "balance": "$6,825.65",
+  "picture": "https://placehold.it/32x32",
+  "age": 24,
+  "name": "Ольга Петрова",
+  "gender": "female",
+  "greeting": "Hello, Ольга! You have 7 unread messages.",
+  "favouriteFruit": "banana"
+};
+
+
+
+let user3 = {
+  "balance": "$11,825.65",
+  "picture": "https://placehold.it/32x32",
+  "age": 22,
+  "name": "Сергей Иванов",
+  "gender": "male",
+  "greeting": "Hello, Сергей! You have 7 unread messages.",
+  "favouriteFruit": "banana"
+};
+
+
+let user4 = {
+  "balance": "$10,825.65",
+  "picture": "https://placehold.it/32x32",
+  "age": 27,
+  "name": "Василий Петров",
+  "gender": "male",
+  "greeting": "Hello, Василий! You have 7 unread messages.",
+  "favouriteFruit": "banana"
+};
+
+let users = [user1, user2, user3, user4];
+
+console.log(showSalary(users, 30)); 
+
+console.log(showSalary(users, 40));
